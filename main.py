8396b5e9ad4989beb14e53afb643d987a7c3a508
hnucamendi <arriolaharold2001@gmail.com>
2022-07-27 22:39:53 -0400

    working connection

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
-import yeelight
+from yeelight import Bulb
+import json
+import time
 
-print(yeelight.discover_bulbs())
+f = open("data.json")
+data = json.load(f)
+
+# print(yeelight.discover_bulbs())
+# print(data[0]["ip"])
+
+firstLight = data[0]["ip"]
+secondLight = data[1]["ip"]
+thirdLight = data[2]["ip"]
+
+bulb1 = Bulb(firstLight)
+bulb2 = Bulb(secondLight)
+bulb3 = Bulb(thirdLight)
+
+def flicker(bulb):
+  for i in range (10):
+    bulb.turn_on()
+    time.sleep(1)
+    bulb.turn_off()
+    time.sleep(1)
+
+
+flicker(bulb1)
+flicker(bulb2)
+flicker(bulb3)
